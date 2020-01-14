a6ab11aca4d3cd56aa34df71e17a774676fc7e08
danieltuwebin <35155091+danieltuwebin@users.noreply.github.com>
2020-01-14 09:26:47 -0500

    Add files via upload

---
+++ config/global.php
@@ -0,0 +1,23 @@
+<?php 
+//Ip de la pc servidor de base de datos
+define("DB_HOST","localhost");
+
+//Nombre de la base de datos
+define("DB_NAME","decoshad_decos");
+
+//Usuario de la base de datos
+define("DB_USERNAME","decoshad_usdeco1");
+
+//Contrase帽a del usuario de la base de datos
+define("DB_PASSWORD","j#@&XHl5cd8^");
+
+//definimos la codificaci贸n de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","decos");
+
+//RUTA DEL SISTEMA EL LOCALHOST O WEB -- SIN (/) SLASH AL FINAL
+define("RUTA","http://decoshadesperu.com/SIGA"); 
+
+?>
